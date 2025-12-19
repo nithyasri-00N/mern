18ea046c6570ae6c9e524af7936b40eac2617744
guru <guruprasanth018@gmail.com>
2025-12-19 12:58:27 +0530

    React- Router Added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Navbar from"./components/Navbar";
+import {Route,Routes} from "react-router-dom";
+import Home from"./pages/Home"
+import About from"./pages/About"
+import Contact from "./pages/Contact"
+import Service from "./pages/Service"
+const App = () => {
+   return(
+    <>
+    {/* <Header/>
+     <Footer/>*/}
+    <Navbar/>
+    <Routes>
+      <Route path="/"element={<Home/>}/>
+      <Route path="/About" element={<About/>}/>
+      <Route path="/Contact" element={<Contact/>}/>
+      <Route path="/services" element={<Service/>}/>
+    </Routes>
+    </>
+  );
+}
+export default App
+
+
+
+
+
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navabar.css'
+const Navbar = () => {
+  return (
+    <div>
+    <nav>
+        <ul className='Navbar'> 
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Service</li>
+        </ul>
+    </nav>
+    </div>
+  )
+}
+
+export default Navbar
